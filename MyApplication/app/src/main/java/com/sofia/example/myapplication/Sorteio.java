2aa4ee3d820264a4002af27b002fc7781ad79e1a
Sofi <sofia.n06@aluno.ifsc.edu.br>
2023-03-03 14:15:55 -0300

    Aula 03/03
    Sorteio

---
+++ MyApplication/app/src/main/java/com/sofia/example/myapplication/Sorteio.java
@@ -0,0 +1,15 @@
+package com.sofia.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Sorteio extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sorteio);
+    }
+}
